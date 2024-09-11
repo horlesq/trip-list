be5a5913e14287aa28a3ea3f8d2dab1ca9ffc381
DESKTOP-LSLTHMA\Horlescu <adrian.horlescu@gmail.com>
2024-09-11 09:03:16 +0300

    Update - implementation and styling

--- src/App.js
+++ src/App.js
@@ -1,3 +1,8 @@
+const initialItems = [
+    { id: 1, description: "Passports", quantity: 2, packed: false },
+    { id: 2, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
     return (
         <div className="app">
@@ -10,7 +15,7 @@ export default function App() {
 }
 
 function Logo() {
-    return <h1>✈️ Trip List 🧳</h1>;
+    return <h1>🧳 Trip List ✈️</h1>;
 }
 
 function Form() {
@@ -22,7 +27,26 @@ function Form() {
 }
 
 function PackingList() {
-    return <div className="list">List</div>;
+    return (
+        <div className="list">
+            <ul>
+                {initialItems.map((item) => (
+                    <Item item={item} />
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+function Item({ item }) {
+    return (
+        <li>
+            <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+                {item.quantity} {item.description}
+            </span>
+            <button>✖️ </button>
+        </li>
+    );
 }
 
 function Stats() {
